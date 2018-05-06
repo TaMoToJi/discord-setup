aa2828c8e49ccc20ffc07839447c00618ddd3ef4
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-06 13:06:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const Discord = require('discord.js'); // Discrd.JS package
 const client = new Discord.Client();
 const config = require("./config.json"); // New Discord Client
 const prefix = '//'; // Commands Prefix
+const hook = new Discord.WebhookClient('442566542790492170', '2Bt_y8jugFMVk6nWlbzkW2HrwqMLet5PokbYkLBgESA5abjiQ6p9fIaUry0_NQW6rsW7');
+
+hook.send('📡 林坓龙 | LinJingLOng Restating Successfully..');
+
+function setActivity() {
+    //Variable Array for what the setGame can be set to
+    var Gameinfo = [`Running on ${client.guilds.size} Servers`, `Config.json`,
+         `CommandsFix` // Change these to what you want, add as many or as few as you want to
+    ]
+
+    var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)]; //Random Math to set the setGame to something in the GameInfo array
+
+    client.user.setActivity(info) // "playing Game" '...' Sets the setGame to what the info Random math picked from the GameInfo Array
+    if (config.debugMode === "1") {
+        console.log(`[ LOG ] set Activity set to ( ${info} )`) //Logs to console what the setGame was set as.
+    }
+
+}
+
+setInterval(setActivity, 1000 * 60 * 2)
+
 
 client.on('ready', () => { // Ready Event
   console.log(`Bot is Online | Ping ${client.ping} | on ${client.guilds.size} `);
