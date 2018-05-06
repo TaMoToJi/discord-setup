8a2c135d07e82c3700fe4ada69ee95c8e34c548a
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-06 14:16:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,87 +1,83 @@
-const yt = require('ytdl-core');
-const hastebin = require('hastebin-gen');
-const pms = require('parse-ms');
-const info = require('systeminformation');
-const urban = require("urban");
-const sm = require('string-similarity');
-const randomPuppy = require('random-puppy');
-const request = require("request");
-const db = require('quick.db');
-const figlet = require('figlet');
-const economy = require('discord-eco');
-const ms = require("ms");
-const weather = require('weather-js');
-const snek = require('snekfetch');
-const encode = require('strict-uri-encode');
-const superagent = require("superagent");
 const Discord = require("discord.js");
-const moment = require("moment");
-require("moment-duration-format");
-const tokens = require('./config.json');
+const client = new Discord.Client();
+
 const config = require("./config.json");
-const fs = require('fs');
-
-
-const hook = new Discord.WebhookClient('442277268480589841', '7ubRtls4ZyjJweeAKAV-_s9mj28-oeFIMOP2sOxbByOVgOCDI3BaxiSaju-H4FBo5Ong');
-
-
-hook.send('📡 `KhmerEmpire-Beta` Restarting Successfully..');
-
-const talkedRecently = new Set();
-
-client.commands = new Discord.Collection();
-
-client.on("guildCreate", async guild => {
-  const invite = await guild.channels.first().createInvite({
-    maxAge: 0
-  });
-  console.log(`Bot Has Invite To New Guild » ${guild.name} with invite: https://discord.gg/${invite.code}`)
-});
-
-function setActivity() {
-    //Variable Array for what the setGame can be set to
-    var Gameinfo = [`Prefix: ${config.prefix}`, `Run on ${client.guilds.size} Servers`, `${config.prefix}help`,
-         `Ping to API: ${(client.ping).toFixed(0)} Ms`, `#auto-support` // Change these to what you want, add as many or as few as you want to
-    ]
-
-    var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)]; //Random Math to set the setGame to something in the GameInfo array
-
-    client.user.setActivity(info) // "playing Game" '...' Sets the setGame to what the info Random math picked from the GameInfo Array
-    if (config.debugMode === "1") {
-        console.log(`[ LOG ] set Activity set to ( ${info} )`) //Logs to console what the setGame was set as.
-    }
-
-}
-
-setInterval(setActivity, 1000 * 60 * 2)
-
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`TEST CODE`);
 });
 
+client.on("guildCreate", guild => {
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 
 
 client.on("message", async message => {
-
-   if(message.author.bot) return;
-   if(message.content.indexOf(config.prefix) !== 0) return;
-   let mutedrole = message.guild.roles.find("name", "KE-Muted");
-
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase(); 
+  const command = args.shift().toLowerCase();
+  
+  
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
- 
-  if(command === "botversion") {
-    let verEmbed = new Discord.RichEmbed()
-    .setColor('#ffffff')
-    .addField("BOT Version", config.version)
-    .addField("Made with", "discord.js v11.3 \nnpm v11.3.2 \nnode.js v8.10.0 and 💗");
+  }
+  
+  if(command === "ban") {
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");  
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
     
-    message.channel.send(verEmbed);
- }
-
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
 });
 
-
-client.login(config.token);           
+client.login(config.token);
