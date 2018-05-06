160c416a753c66890a030044c8c10a4de344e2d1
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-06 12:21:40 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# discord-setup
+# YouTube Tutorial Bot Series
+
+Make sure to subscribe to my channel.
+[YouTube Channel](https://www.youtube.com/channel/UCbKKNU5X3NzhQztp22-f8kw)
+
+Also, join my Discord server.
+
+[Discord Server](https://discord.gg/kweHZWg)
---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js'); // Discrd.JS package
+const client = new Discord.Client(); // New Discord Client
+const prefix = '!'; // Commands Prefix
+
+client.on('ready', () => { // Ready Event
+  console.log('Bot has started!');
+});
+
+client.on('message', message => { // Message Event
+  
+  if (message.author.bot) return undefined; // Bot doesn't reply to itself
+  
+  let msg = message.content.toLowerCase(); // Message's content to lowercase letter
+  let args = message.content.slice(prefix.length).trim().split(' '); // Arguments 
+  let command = args.shift().toLowerCase(); // Shift arguments to lower case
+  
+  try {
+    let commands = require(`./commands/${command}.js`);
+    commands.run(client, message, args);
+  } catch (e) {
+    console.log(e.stack)
+  } finally {
+    console.log(command)
+  }
+});
+
+client.login(process.env.SECRET); // My token is hidden
